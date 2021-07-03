aeafee4122e967c4dc066cc4fa74033444f9d0cc
변웅섭 <woungsub1234@gmail.com>
2021-07-03 22:09:46 +0900

    Optional binding 관련 주석 추가

--- OurToDoList/OurToDoList/ViewController.swift
+++ OurToDoList/OurToDoList/ViewController.swift
@@ -32,12 +32,15 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     @objc private func didTapAdd() {
+
         let alert = UIAlertController(title: "New Item", message: "Enter new to do list item", preferredStyle: .alert)
         alert.addTextField{field in field.placeholder = "Enter item..." }
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (_) in
+            // Optional binding(옵셔널 바인딩): 변수의 값이 nil이라면 통과하고 값이 존재한다면 변수에 값을 대입합니다.
+            
             if let field = alert.textFields?.first {
-                if let text = field.text, !text.isEmpty {
+                if let text = field.text, !text.isEmpty { // 왼쪽의 코드와 같이 Optional Binding과 조건절이 같이 쓰였을 경우에는 Biniding이 일어난 후에 실행되기 때문에 즉, 옵셔널이 벗겨진 값을 가지고 검사한다.
                     // Enter new to do list item
                     DispatchQueue.main.async {
                         var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
@@ -49,6 +52,7 @@ class ViewController: UIViewController, UITableViewDataSource {
                     
                 }
             }
+            
         }))
         
         present(alert, animated: true)
