a7eb4c83a0b6c8b2f448c88f067d16083026692f
변웅섭 <woungsub1234@gmail.com>
2021-07-07 23:23:03 +0900

    Add MovieSearchApp

---
+++ MovieSearcher/MovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearcher
+//
+//  Created by 변웅섭 on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
