56cbcd8f90e01376b345a4dfeace7bd07e666b13
변웅섭 <woungsub1234@gmail.com>
2021-07-08 09:57:35 +0900

    Init CovidTracker project

---
+++ COVID Tracker/COVID Tracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  COVID Tracker
+//
+//  Created by 변웅섭 on 2021/07/08.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
