930e98c95b0bc0f190f68968ae7c105e808672af
변웅섭 <woungsub1234@gmail.com>
2021-07-13 14:20:27 +0900

    Cocoapods Firebase 설정 추가

--- instagram/instagram/Resources/AppDelegate.swift
+++ instagram/instagram/Resources/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 변웅섭 on 2021/07/12.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
