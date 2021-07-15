690e0032e2d8c75e2f86cde385e237fe7999e30e
변웅섭 <woungsub1234@gmail.com>
2021-07-15 12:03:34 +0900

    Add Logic on LoginViewController

--- instagram/instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ instagram/instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -193,6 +193,33 @@ class LoginViewController: UIViewController {
         }
         
         // login functionality
+        
+        var username: String?
+        var email: String?
+        
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            // email
+            email = usernameEmail
+        }
+        else {
+            // username
+            
+        }
+        
+        AuthManager.shared.loginUser( username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    // user logged in
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    // error occurred
+                    let alert = UIAlertController(title: "Log In Error", message: "We were unable to log you in", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+
+        }
     }
     
     @objc private func didTapTermsButton() {
