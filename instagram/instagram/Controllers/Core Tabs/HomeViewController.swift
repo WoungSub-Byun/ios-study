bd3aca5a8d726d056abe94251e69f8555c9fbf71
변웅섭 <woungsub1234@gmail.com>
2021-07-27 22:55:04 +0900

    Add TableView on HomeVC

--- instagram/instagram/Controllers/Core Tabs/HomeViewController.swift
+++ instagram/instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -9,9 +9,25 @@ import Firebase
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(IGFeedPostTableViewCell.self,
+                           forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -39,3 +55,15 @@ class HomeViewController: UIViewController {
     }
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+        return cell
+    }
+}
