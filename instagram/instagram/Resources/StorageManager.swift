4a9fa54001ac9f6fa2bbc847651634f684e5da5a
변웅섭 <woungsub1234@gmail.com>
2021-07-28 00:14:02 +0900

    Add methods on StorageManager

--- instagram/instagram/Resources/StorageManager.swift
+++ instagram/instagram/Resources/StorageManager.swift
@@ -8,7 +8,38 @@
 import FirebaseStorage
 
 public class StorageManager {
+    
     static let shared = StorageManager()
     
+    private let bucket = Storage.storage().reference()
+    
+    public enum IGStorageManagerError: Error {
+        case failedToDownload
+    }
+    
     // MARK: - Public
+    
+    public func uploadUserPost(model: UserPost, completion: @escaping (Result<URL, Error>) -> Void) {
+        
+    }
+    
+    public func downloadImage(with reference: String, completion: @escaping (Result<URL, IGStorageManagerError>) -> Void) {
+        bucket.child(reference).downloadURL(completion: { url, error in
+            guard let url = url, error == nil else {
+                completion(.failure(.failedToDownload))
+                return
+            }
+            
+            completion(.success(url))
+        })
+    }
+    
+}
+
+public enum UserPostType {
+    case photo, video
+}
+
+public struct UserPost {
+    let postType: UserPostType
 }
