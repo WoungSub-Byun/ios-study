106653eaa92adc929aad76f344f1b73318c0de24
변웅섭 <woungsub1234@gmail.com>
2021-07-28 13:26:21 +0900

    Add AlertController on EditProfileVC

--- instagram/instagram/Controllers/Other/General/EditProfileViewController.swift
+++ instagram/instagram/Controllers/Other/General/EditProfileViewController.swift
@@ -11,8 +11,37 @@ class EditProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapSave))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(didTapCancel))
     }
-    
+ 
+    @objc private func didTapSave() {
+        
+    }
+    @objc private func didTapCancel() {
+        
+    }
+    @objc private func didTapChangeProfilePicture() {
+        let actionSheet = UIAlertController(title: "Profile Picture",
+                                            message: "Change profile picture", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {_ in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose from Library", style: .default, handler: {_ in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil ))
+        
+        actionSheet.popoverPresentationController?.sourceView = view
+        actionSheet.popoverPresentationController?.sourceRect = view.bounds
+        
+        present(actionSheet, animated: true)
+    }
 }
