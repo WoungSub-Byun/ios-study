30a58cf97862d5978725f00ff08b0c1b0bbee0c2
변웅섭 <woungsub1234@gmail.com>
2021-07-29 17:26:59 +0900

    Add CollectionView Headers

---
+++ instagram/instagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  instagram
+//
+//  Created by 변웅섭 on 2021/07/29.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
+}
---
+++ instagram/instagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileTabsCollectionReusableView.swift
+//  instagram
+//
+//  Created by 변웅섭 on 2021/07/29.
+//
+
+import UIKit
+
+class ProfileTabsCollectionReusableView: UICollectionReusableView {
+    static let identifier = "ProfileTabsCollectionReusableView"
+}
