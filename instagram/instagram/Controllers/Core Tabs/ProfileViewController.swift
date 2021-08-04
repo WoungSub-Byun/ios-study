a624c86e262fee7bf3f7315039300e96488138a8
변웅섭 <woungsub1234@gmail.com>
2021-08-04 16:59:05 +0900

    Add header and footer on collectionView in ProfileVC

--- instagram/instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ instagram/instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -18,8 +18,11 @@ final class ProfileViewController: UIViewController {
         
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: 1)
+        let size = (view.width - 4 ) / 3
+        layout.itemSize = CGSize(width: size, height: size)
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: layout)
         collectionView?.backgroundColor = .red
@@ -65,15 +68,54 @@ final class ProfileViewController: UIViewController {
 }
 
 extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 0
+        }
         return 30
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
-        cell.backgroundColor = .systemBlue
+        cell.configure(debug: "test")
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard kind == UICollectionView.elementKindSectionHeader else {
+            // footer
+            return UICollectionReusableView()
+        }
+        
+        if indexPath.section == 1 {
+             // tabs header
+            let tapControlHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                         withReuseIdentifier: ProfileTabsCollectionReusableView.identifier,
+                                                                         for: indexPath ) as! ProfileTabsCollectionReusableView
+            return tapControlHeader
+        }
+        
+        
+        let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                     withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier,
+                                                                     for: indexPath ) as! ProfileInfoHeaderCollectionReusableView
+        return profileHeader
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize(width: collectionView.width,
+                          height: collectionView.height / 3)
+        }
+        
+        // Size of section Taps
+        return CGSize(width: collectionView.width,
+                      height: 65)
     }
 }
